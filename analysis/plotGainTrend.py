1338a5de72319ac67e55d54051ce2f7102f1541d
xpol <xpol@pcxipe1>
2017-02-06 18:15:57 +0100

    moving fit to a function and time offset to header

--- analysis/plotGainTrend.py
+++ analysis/plotGainTrend.py
@@ -3,6 +3,30 @@ import numpy as np
 import sys, os
 import argparse
 
+#
+# useful function here:
+#
+
+G2FWHM = 2.3548200450309493
+def fitSimpleGauss(histo, nsigma=1.5, verbose=True):
+    g0 = ROOT.TF1("g0", "gaus", 500, 10000)
+    histo.Fit("g0", "RNQ")
+    g = ROOT.TF1("g", "gaus", \
+                 g0.GetParameter(1) - nsigma*g0.GetParameter(2),\
+                 g0.GetParameter(1) + nsigma*g0.GetParameter(2))
+    histo.Fit("g", "RQ")
+    gPeak  = g.GetParameter(1)
+    gSigma = g.GetParameter(2)
+    gFWHM  = G2FWHM*(gSigma/gPeak)
+    gPeakErr = g.GetParError(1)
+    gFWHMErr = gFWHM*np.sqrt((g.GetParError(2)/gSigma)**2 + (gPeakErr/gPeak)**2)
+    
+    if verbose:
+        print  "fitSimpleGauss:", gPeak, gSigma, "Eres=", gSigma/gPeak, \
+            "FWHM", gFWHM
+    return (histo, gPeak, gFWHM, gPeakErr, gFWHMErr)
+    
+# 
 formatter = argparse.ArgumentDefaultsHelpFormatter
 parser = argparse.ArgumentParser(formatter_class=formatter)
 parser.add_argument('infile', type=str, nargs='+',
@@ -27,13 +51,13 @@ SaveOutput = args.saveout
 nsigfit  = 1.5
 CUT      = '(%s)' %args.cut
 minEvtInHist = 500
-useFWHM      = True
+
 
 # set root style
 ROOT.gStyle.SetOptFit(1)
 ROOT.gStyle.SetPadGridX(True)
 ROOT.gStyle.SetPadGridY(True)
-G2FWHM = 2.3548200450309493
+
 
 
 tt = ROOT.TChain('tree')
@@ -86,7 +110,7 @@ ctmp.Print(allHistoFileName +"[");
 hAll.Draw("colz")
 ctmp.Print(allHistoFileName);
 
-g0 = ROOT.TF1("g0", "gaus", 3000, 5500)
+###g0 = ROOT.TF1("g0", "gaus", 3000, 5500)
 for i in xrange(Nbins):
     # get projection in time (y coord in hAll)
     htmp = hAll.ProjectionX("htmp", i+1,i+1)
@@ -94,25 +118,17 @@ for i in xrange(Nbins):
     if htmp.GetEntries() >= minEvtInHist:
         #print '---> ', htmpnp.GetEntries(), htmp.GetEntries(),\
         #    htmp.GetEntries()/htmpnp.GetEntries()
+
+        (htmp, gPeak, gFWHM, gPeakErr, gFWHMErr) = \
+        fitSimpleGauss (htmp,  nsigfit)
         
-        htmp.Fit("g0", "RNQ")
-        g = ROOT.TF1("g", "gaus", \
-                     g0.GetParameter(1) - nsigfit*g0.GetParameter(2),\
-                     g0.GetParameter(1) + nsigfit*g0.GetParameter(2))
-        htmp.Fit("g", "RQ")
         timeX.append(timeVal[i])
         timeXErr.append(timeErr[i])
-        gPeak  = g.GetParameter(1)
-        gSigma = g.GetParameter(2)
-        print  "\n",i, gPeak, gSigma, "Eres=", gSigma/gPeak, \
-            "FWHM", 2.355*(gSigma/gPeak)
         gainVal.append(gPeak)
-        gainErr.append(g.GetParError(1))
-        resVal.append(100.*gSigma/gPeak)
-        resErr.append(resVal[-1]*(g.GetParError(2)/gSigma )) # ~ok for now
-        if useFWHM:
-            resVal[-1] = resVal[-1]*G2FWHM
-            resErr[-1] = resErr[-1]*G2FWHM
+        gainErr.append(gPeakErr)
+        resVal.append(gFWHM)
+        resErr.append(gFWHMErr)
+
     ROOT.gPad.Update()
     ctmp.Print(allHistoFileName);
 
@@ -143,9 +159,6 @@ gGain.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M:%S}");
 gGain.GetXaxis().SetTimeOffset(TimeOffset)
 gGain.GetXaxis().SetLabelOffset(0.03)
 gGain.Draw("ap")
-#fGain = ROOT.TF1('fGain', 'pol1', 0, (SpotCut_tthr-TimeOffset))
-#fGain.SetParameters(10, 0.002)
-#gGain.Fit("fGain", "R")
 ROOT.gPad.Update()
 try:
     psGain = gGain.GetListOfFunctions().FindObject("stats");
@@ -158,15 +171,10 @@ except:
 cTrend.cd(2)
 gRes = ROOT.TGraphErrors(N, timeX, resVal, timeXErr, resErr)
 gRes.SetMarkerStyle(20)
-gRes.SetTitle("Resolution (gaussian sigma/peak) @ 5.9 keV (%s)" % Label)
-if useFWHM:
-    gRes.SetTitle("Resolution (gaussian sigma FWHM) @ 5.9 keV (%s)" % Label)
+gRes.SetTitle("Resolution (gaussian sigma FWHM) @ 5.9 keV (%s)" % Label)
 gRes.GetXaxis().SetTitle("Elapsed Time (hours)")
 gRes.GetYaxis().SetTitle("#Delta E/E (%)")
 gRes.Draw("ap")
-#fRes = ROOT.TF1('fRes', 'pol1', 0, (SpotCut_tthr-TimeOffset)/3600.)
-#fRes.SetParameters(0.1, 0.001)
-#gRes.Fit("fRes", "R")
 ROOT.gPad.Update()
 try:
     psRes = gRes.GetListOfFunctions().FindObject("stats");
@@ -188,10 +196,11 @@ if SaveOutput:
     outFile = file(outFileName, "w+")
     outFile.write("# Gain and resolution for %s \n" %Label)
     outFile.write("# Written on %s\n" % time.ctime())
+    outFile.write("# Time offset (hr) = %f\n" % TimeOffset)
     outFile.write("# time (hr)\tpeak\tfwhm\ttimeErr\tpeakErr\tfwhmErr\n")
     for i in xrange(N):
         outFile.write("%g\t%g\t%g\t%g\t%g\t%g\n" %\
-                      (timeX[i]+TimeOffset, gainVal[i], resVal[i],
+                      (timeX[i], gainVal[i], resVal[i],
                        timeXErr[i], gainErr[i], resErr[i]))
     
     outFile.close()

689b222b13fb2f9f13db92c9e28e29fc84ce22a8
pesce <melissa.pesce.rollins@pi.infn.it>
2017-02-07 15:50:51 +0100

    Implemented the root style to the plots.

--- analysis/parse_dict.py
+++ analysis/parse_dict.py
@@ -2,7 +2,7 @@ from  gem_run_dict import GEM_RUN_DICT as dictionary
 
 import os
 import ROOT
-
+import __root__
 
 def parse_and_fit():
     for gem in dictionary.keys():
@@ -24,12 +24,18 @@ def parse_and_plot2d():
         main_peak_label = "MainPeak_%s"%gem
         c1 = ROOT.TCanvas(fwhm_label)
         fwhm = f.Get(fwhm_label)
+        fwhm.SetTitle(fwhm_label)
+        fwhm.GetYaxis().SetTitle("fBaricenterY[0]")
+        fwhm.GetXaxis().SetTitle("fBaricenterX[0]")
         fwhm.Draw("colz")
         c1.Update()
         c1.SaveAs("%s.png"%fwhm_label)
         raw_input()
         c2 = ROOT.TCanvas(main_peak_label)
         peak = f.Get(main_peak_label)
+        peak.SetTitle(main_peak_label)
+        peak.GetYaxis().SetTitle("fBaricenterY[0]")
+        peak.GetXaxis().SetTitle("fBaricenterX[0]")
         peak.Draw("colz")
         c2.SaveAs("%s.png"%main_peak_label)
         raw_input()

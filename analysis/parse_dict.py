8b7478833ce6a751a7fdadbb1bb7b4108d9526af
xpol <xpol@pcxipe1>
2017-02-07 14:59:07 +0100

    Simple script to parse the gem dictionary and run the analysis chain of creating the cube file and the 2d uniformity maps. They ae saved in a root file

---
+++ analysis/parse_dict.py
@@ -0,0 +1,39 @@
+from  gem_run_dict import GEM_RUN_DICT as dictionary
+
+import os
+import ROOT
+
+
+def parse_and_fit():
+    for gem in dictionary.keys():
+        file_str = ''
+        for run in dictionary[gem]['unif_illumination_runs']:
+            filepath = '../out/001_0000%s_data_TH5_outputfile.root'%run
+            file_str+='%s '%filepath
+       
+        output_file_path = '%s_uniformities.root'%gem
+        cmd = 'python plotGEMUniformMap.py %s -o %s -l %s'%(file_str, output_file_path, gem)
+        os.system(cmd)
+
+
+def parse_and_plot2d():
+    for gem in dictionary.keys():
+        output_file_path = '%s_uniformities.root'%gem
+        f = ROOT.TFile(output_file_path)
+        fwhm_label = "FWHM_%s"%gem
+        main_peak_label = "MainPeak_%s"%gem
+        c1 = ROOT.TCanvas(fwhm_label)
+        fwhm = f.Get(fwhm_label)
+        fwhm.Draw("colz")
+        c1.Update()
+        c1.SaveAs("%s.png"%fwhm_label)
+        raw_input()
+        c2 = ROOT.TCanvas(main_peak_label)
+        peak = f.Get(main_peak_label)
+        peak.Draw("colz")
+        c2.SaveAs("%s.png"%main_peak_label)
+        raw_input()
+
+
+
+parse_and_plot2d()

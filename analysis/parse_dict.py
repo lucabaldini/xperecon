965838d0a4b7ddb6d66f5389bda5fa53fddf3ea6
xpol <xpol@pcxipe1>
2017-02-08 15:38:08 +0100

    Added the method to fit gain peak.

--- analysis/parse_dict.py
+++ analysis/parse_dict.py
@@ -3,6 +3,8 @@ from  gem_run_dict import GEM_RUN_DICT as dictionary
 import os
 import ROOT
 import __root__
+import numpy
+
 
 def parse_and_fit():
     for gem in dictionary.keys():
@@ -16,10 +18,50 @@ def parse_and_fit():
         os.system(cmd)
 
 
+def fit_gain_trend(filepath, gem_label):
+    time, peak, fwhm, timeErr, peakErr, fwhmErr = \
+                        numpy.loadtxt(filepath, delimiter='\t', unpack=True)
+    
+    from matplotlib import pyplot as plt
+    print "Ploting peak position for %s"%gem_label
+    plt.errorbar(time, peak, xerr=timeErr, yerr=peakErr, marker='o',ls='none')
+    pol_coef = numpy.polyfit(time, peak,deg=5)
+
+    def time_trend_expr(pol_coef, offset, time_var='fTimeStamp'):
+        """
+        """
+        expr = ''
+        degree = len(pol_coef)
+        for i, p in enumerate(pol_coef):
+            if i < degree - 1:
+                expr += '%f*(%s - %f)**%d + ' %\
+                        (p, time_var, offset, degree - i - 1)
+            else:
+                expr += '%f' % p
+        expr = '(%s)/%f' % (expr, p) 
+        return expr
+
+    offset = 1485500048.003290
+    print time_trend_expr(pol_coef, offset)
+    
+    plt.plot(time, numpy.polyval(pol_coef, time))
+    print "Here are the polynomial coefficients for the fir with pol degree 5",pol_coef
+    plt.show()
+
+    
+    
+    #raw_input()
+        
 def parse_and_plot2d():
-    for gem in dictionary.keys():
-        output_file_path = '%s_uniformities.root'%gem
-        f = ROOT.TFile(output_file_path)
+    #for gem in dictionary.keys():
+    for gem in ['gem_1420']:
+        root_file_path = '%s_uniformities.root'%gem
+        gain_trend_file_path = '/data/xpe/xperecon/analysis/gemNov2016Summary/gaintrend_%s.data'%gem
+        #if gem=='gem_1420':
+        #
+        fit_gain_trend(gain_trend_file_path, gem)
+        
+        f = ROOT.TFile(root_file_path)
         fwhm_label = "FWHM_%s"%gem
         main_peak_label = "MainPeak_%s"%gem
         c1 = ROOT.TCanvas(fwhm_label)
@@ -30,7 +72,7 @@ def parse_and_plot2d():
         fwhm.Draw("colz")
         c1.Update()
         c1.SaveAs("%s.png"%fwhm_label)
-        raw_input()
+        #raw_input()
         c2 = ROOT.TCanvas(main_peak_label)
         peak = f.Get(main_peak_label)
         peak.SetTitle(main_peak_label)
@@ -38,7 +80,7 @@ def parse_and_plot2d():
         peak.GetXaxis().SetTitle("fBaricenterX[0]")
         peak.Draw("colz")
         c2.SaveAs("%s.png"%main_peak_label)
-        raw_input()
+        #raw_input()
 
 
 
